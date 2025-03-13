5f7b49055cd5e8e41b928889d73082c356517ddd
jill64 <contact@jill64.dev>
2025-03-13 23:16:27 +0900

    fix(eslint): update svelteTsConfig to ignore specific directories and files

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,6 @@
 import { svelteTsConfig } from '@jill64/eslint-config-svelte'
 
 /** @type {import('@jill64/eslint-config-ts').FlatConfig[]} */
-export default svelteTsConfig()
+export default svelteTsConfig({
+  ignores: ['.svelte-kit', '**/*.svelte.ts']
+})
